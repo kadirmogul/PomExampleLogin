2c1920c5e0e61caac2da209b510e1869650a97c3
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-11 15:02:51 +0300

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
 
 public class BaseTest {
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeClass
+    public void setUp(){
+       WebDriverManager.chromedriver().setup();
+       driver=new ChromeDriver();
+       loginPage=new LoginPage(driver);
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+       driver.manage().window().maximize();
+   }
+   @AfterClass
+    public void tearDown(){
+        driver.quit();
+   }
 }
