1c3dee2648f3191ec637b241a7b7d0c9882cd40f
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-11 15:03:01 +0300

    Update LoginPage.java

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
+    private WebDriver driver;
+
+    private By usernameTextLocator=By.id("username");
+    private By userPasswordTextLocator=By.id("password");
+    private By buttonClickLocator=By.id("submit");
+
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void enterUserName(String username){
+        driver.findElement(usernameTextLocator).sendKeys(username);
+    }
+    public void enterUserPassword(String password){
+        driver.findElement(userPasswordTextLocator).sendKeys(password);
+    }
+    public void submitButtonClick(){
+        driver.findElement(buttonClickLocator).click();
+    }
 }
