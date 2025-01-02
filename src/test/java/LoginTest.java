688d2a302f008a38e769fea8b39b3d8a42fad90a
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2025-01-02 10:52:17 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
+    @Test
+    public void loginTest(){
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        loginPage.enterUserName("student");
+        loginPage.enterUserPassword("Password123");
+        loginPage.submitButtonClick();
+    }
 }
